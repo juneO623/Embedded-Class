593ca774901ec80788cbb6c52a9bbd0f2de57641
username <81845513+june3011@users.noreply.github.com>
2022-12-01 11:22:37 +0900

    폴더 구조 변경 및 Add : udp C# 코드

---
+++ 프실 두번째수행평가/udp_winform/udp_winform/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace udp_winform
+{
+    public partial class Form1 : Form
+    {
+
+        char[] MyIp = new char[13];
+
+        private SerialPort mySerial = new SerialPort();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            byte[] data = new byte[1024];
+            //"10.80.163.244"
+            UdpClient client = new UdpClient("192.168.139.128", 10004);
+
+            data = Encoding.UTF8.GetBytes("haha");
+            client.Send(data, data.Length);
+
+            client.Close();
+            Console.WriteLine("Stopping clinet");
+        }
+
+        private void button1_Click(object sender, EventArgs e)  // LED1 ON
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)  // LED2 ON
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)  // LED1 OFF
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)  // LED2 OFF
+        {
+
+        }
+    }
+}
